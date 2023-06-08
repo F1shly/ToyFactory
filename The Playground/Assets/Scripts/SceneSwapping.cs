265686cdc77033c88bddc3225c622251e63ed02b
Jake <j4k3.ow@gmail.com>
2023-06-08 15:00:51 +0100

    moving stuff around and can now go from scnen to scene

---
+++ The Playground/Assets/Scripts/SceneSwapping.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwapping : MonoBehaviour
+{
+    public bool next;
+    public int targetScene;
+    GameObject player;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == player.GetComponent<Collider>())
+        {
+            LoadNextLevel();
+        }
+    }
+
+
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(targetScene);
+    }
+
+    private void Update()
+    {
+        if (next)
+        {
+            LoadNextLevel();
+        }
+    }
+}
