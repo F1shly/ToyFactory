19614e350d9349df889f5a3e335aa59458d7b67e
Jake <j4k3.ow@gmail.com>
2023-07-21 20:51:50 +0100

    scuffed commit

---
+++ The Playground/Assets/Scenes/testing randomness/spawnRoom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnRoom : MonoBehaviour
+{
+    public GameObject[] obj;
+
+    private void Start()
+    {
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Random.Range(0, 100), 0, Random.Range(0, 100)), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Random.Range(0, 100), 0, Random.Range(0, 100)), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Random.Range(0, 100), 0, Random.Range(0, 100)), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Random.Range(0, 100), 0, Random.Range(0, 100)), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Random.Range(0, 100), 0, Random.Range(0, 100)), gameObject.transform.rotation);
+    }
+}
