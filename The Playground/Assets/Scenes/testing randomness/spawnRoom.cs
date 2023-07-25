27e523015f6d4c68cb5f73afd62a381c20d4ac23
Jake <j4k3.ow@gmail.com>
2023-07-25 14:47:19 +0100

    put stuff in

--- The Playground/Assets/Scenes/testing randomness/spawnRoom.cs
+++ The Playground/Assets/Scenes/testing randomness/spawnRoom.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class spawnRoom : MonoBehaviour
 {
     public GameObject[] obj;
+    public int y = 50, z = -70, x = -70;
 
     private void Start()
     {
-        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(50,0,0), gameObject.transform.rotation);
-        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Mathf.Round(Random.Range(5, 10)) * 10, 0, Mathf.Round(Random.Range(1, 3)) * 10), gameObject.transform.rotation);
-        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Mathf.Round(Random.Range(0, 6)) * 10, 0, Mathf.Round(Random.Range(2, 4)) * 10), gameObject.transform.rotation);
-        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Mathf.Round(Random.Range(2, 8)) * 10, 0, Mathf.Round(Random.Range(4, 7)) * 10), gameObject.transform.rotation);
-        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(50,0,100), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(x - x,y,z), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Mathf.Round(Random.Range(0, 70)), y, Mathf.Round(Random.Range(0, 45)) + z), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Mathf.Round(Random.Range(-70, -10)), y, Mathf.Round(Random.Range(25, 65)) + z), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(Mathf.Round(Random.Range(-35, 50)), y, Mathf.Round(Random.Range(65, 85)) + z), gameObject.transform.rotation);
+        Instantiate(obj[Random.Range(0, obj.Length)], new Vector3(x - x,y,-z), gameObject.transform.rotation);
     }
 }
